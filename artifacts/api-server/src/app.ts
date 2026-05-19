979ae1ce80655b2084d2aea3ebdbb0649e9202b4
zbala771 <valverde771cf@gmail.com>
2026-05-19 09:23:59 -0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,18 +1,9 @@
 import express from 'express'
-const pinoHttp = require('pino-http')
 
 const app = express()
 
-app.use(pinoHttp())
-
 app.get('/', (req, res) => {
   res.send('API online')
-})
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
 })
 
 export default app
