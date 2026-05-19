fefdd1753a336e2cec7b70a01de41f9fcef41057
zbala771 <valverde771cf@gmail.com>
2026-05-18 23:40:51 -0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,10 +1,11 @@
-import express, { type Express } from "express";
+import * as express from "express";
+import type { Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
-const app: Express = express();
+const app = express();
 
 app.use(
   pinoHttp({
